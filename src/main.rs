f8e9d60c8c04ec882bb3209395f192391dfdd217
Martin Pool <mbp@sourcefrog.net>
2023-02-03 07:24:05 -0800

    Verbose trace to stderr

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 // Copyright 2023 Martin Pool
 
 use std::fs::read_to_string;
+use std::io::stderr;
+
+use tracing::{trace, Level};
+use tracing_subscriber::prelude::*;
 
 use iamthat::policy;
 
+fn init_tracing() {
+    tracing_subscriber::fmt()
+        .with_writer(stderr)
+        .with_max_level(Level::TRACE)
+        .without_time()
+        .finish()
+        .init();
+    trace!("Tracing installed");
+}
+
 fn main() {
-    tracing_subscriber::fmt::init();
+    init_tracing();
 
     let policy_json = read_to_string("example/resource_policy/s3_list.json").unwrap();
     let policy: policy::Policy = serde_json::from_str(&policy_json).unwrap();
