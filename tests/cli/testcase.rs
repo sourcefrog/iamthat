76fd1ba86ab4ded05a1d8be4fb02b8b291875fc6
Martin Pool <mbp@sourcefrog.net>
2023-05-16 06:36:06 -0700

    Test all examples

--- tests/cli/testcase.rs
+++ tests/cli/testcase.rs
@@ -2,11 +2,13 @@
 
 //! Tests for the `iamthat test` command.
 
-use assert_cmd::prelude::*;
+// use assert_cmd::prelude::*;
+use glob::glob;
 use predicates::prelude::*;
 
 use super::*;
 
+// A redundant test of just one example, just for an easy start.
 #[test]
 fn s3_basics() {
     run()
@@ -17,4 +19,16 @@ fn s3_basics() {
             predicate::str::contains("Assertion 0 passed")
                 .and(predicate::str::contains("Assertion 1 passed")),
         );
+}
+
+#[test]
+fn all_example_testcases() {
+    let paths = glob("example/testcase/*.json")
+        .expect("Glob testcases")
+        .map(|r| r.expect("Read testcase name"))
+        .collect::<Vec<_>>();
+    run().args(["test"]).args(paths).assert().success().stderr(
+        predicate::str::contains("Assertion 0 passed")
+            .and(predicate::str::contains("Assertion 1 passed")),
+    );
 }
